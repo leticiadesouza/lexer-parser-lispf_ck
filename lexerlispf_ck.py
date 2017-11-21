0ac6e728592bad316e2ae3c71405438eea63c2f0
Leticia de Souza <leticia.souza212@gmail.com>
2017-11-21 19:29:07 -0200

    Add file test and modify some lexer rules

--- lexerlispf_ck.py
+++ lexerlispf_ck.py
@@ -5,26 +5,54 @@ lexer = ox.make_lexer(lexer_rules)
 
 lexer_rules = [
     ('NAME', r'[a-zA-Z]+'),
-    ('NUMBER', r'\d+(\.\d)'),
+    ('NUMBER', r'\d+'),
+    ('RIGHT', r'right'),
+    ('LEFT', r'left'),
     ('OPEN_PAREN', r'\('),
     ('CLOSE_PAREN', r'\)'),
+    ('INCR', r'incr'),
     ('COMMA', r'\,')
     ('SPACE', r'\s+'),
+    ('NEWLINE', r'\n'),
+    ('DEF', r'def'),
 ]
 
 lexer = ox.make_lexer(lexer_rules)
 
-token_list = ['NUMBER', 'NAME', 'OPEN_PAREN', 'CLOSE_PAREN', 'COMMA']
+token_list = [
+    'NAME',
+    'NUMBER',
+    'RIGHT',
+    'LEFT',
+    'OPEN_PAREN',
+    'CLOSE_PAREN',
+    'INCR',
+    'COMMA',
+    'SPACE',
+    'NEWLINE',
+    'DEF'
+]
 
 parser = ox.make_parser(parser_rules)
 
-parser_rules = [
+parser = ox.make_parser([
 
-    (),
+],    token_list)
 
-]
+
+
+lisp_entry = []
+for data in source:
+    lisp_entry = []
+    for data in source:
+        lisp_entry.append(data)
+
+tokens = lexer(lisp_entry)
+print('Tokens:\n', tokens)
 
 @click.command()
 @click.argument()
 
-def parser()
+def entry_tree(source):
+    program = source.read()
+    print()
